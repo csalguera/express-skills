ca34220793c66681edd6113421b841770de1b059
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-05 18:26:47 -0500

    refactor code to utilize database from mongoDB instead of local data directory

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,15 @@
-import { skillsArr } from '../data/skills-data.js'
+import { Skill } from "../models/skill.js"
 
 function index(req, res) {
-  res.render('skills/index', {
-    title: 'Skills List',
-    skills: skillsArr
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      title: 'Skills List',
+      skills: skills
+    })
+  }).catch(error => {
+    console.log(error);
+    res.redirect('/')
   })
 }
 

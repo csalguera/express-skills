d46f3010b031cca96dff028bd43054d6092cc40f
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-04 21:39:32 -0500

    add array to skills-data file and access that data within views/skills/index file

---
+++ data/skills-data.js
@@ -0,0 +1,16 @@
+const skillsArr = [
+  {skill: 'For Loops', category: 'Frontend', learned: true, isPlural: true},
+  {skill: 'Array Iterator Methods', category: 'Frontend', learned: true, isPlural: true},
+  {skill: 'DOM Manipulation', category: 'Frontend', learned: true, isPlural: false},
+  {skill: 'Flexbox', category: 'Frontend', learned: true, isPlural: false},
+  {skill: 'Keyframes', category: 'Frontend', learned: true, isPlural: true},
+  {skill: 'Node.js', category: 'Backend', learned: false, isPlural: false},
+  {skill: 'Express.js', category: 'Backend', learned: false, isPlural: false},
+  {skill: 'MongoDB', category: 'Backend', learned: false, isPlural: false},
+  {skill: 'Mongoose', category: 'Backend', learned: false, isPlural: false},
+  {skill: 'React.js', category: 'Frontend', learned: false, isPlural: false},
+]
+
+export {
+  skillsArr
+}
--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+import { skillsArr } from '../data/skills-data.js'
 
 const router = Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('skills/index', { title: 'Skills List' })
+  res.render('skills/index', {
+    title: 'Skills List',
+    skills: skillsArr
+  })
 })
 
 export {
